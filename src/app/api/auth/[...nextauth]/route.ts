0d5ebae6f744b52d082c6cbd720bdde4e2833184
D-FOG <ogbondafavour9@gmail.com>
2025-04-15 21:08:44 +0100

    fix: my-meeting/route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,5 @@ import NextAuth from "next-auth"
 import { authOptions } from "@/lib/authOptions"
 
 const handler = NextAuth(authOptions)
-const auth = NextAuth(authOptions)
 
-export { handler as GET, handler as POST, auth }
+export { handler as GET, handler as POST }
